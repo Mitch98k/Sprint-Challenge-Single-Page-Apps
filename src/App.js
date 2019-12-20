759fe9d53b057d299876444dce3c8f1f94633fa6
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2019-12-20 13:42:50 -0700

    I got the search bar to work.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Header from "./components/Header.js";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import WelcomePage from "./components/WelcomePage.js";
 import CharacterList from "./components/CharacterList";
+import SearchForm from "./components/SearchForm";
 export default function App() {
   return (
     <main>
       <Header /> 
+      <SearchForm />
+      <div>
       <Router>
-          <Route exact path = "/Characters">
-            <CharacterList />
-          </Route>
-          <Route exact path = "/">
-            <WelcomePage />
-          </Route>
+      <Route exact path = "/" component = {WelcomePage}/>
+      <Route exact path = "/Characters" component = {CharacterList}/>
       </Router>
+      </div>
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,54 @@
-import React, { useState } from "react";
+import React, {useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const characters = [
+"Rick Sanchez",
+"Morty Smith",
+"Summer Smith",
+"Beth Smith",
+"Jerry Smith",
+"Abadango Cluster Princess",
+"Abradolf Lincler",
+"Adjudicator Rick",
+"Agency Director",
+"Alan Rails",
+"Albert Einstein",
+"Alexander",
+"Alien Googah",
+"Alien Morty",
+"Alien Rick",
+"Amish Cyborg",
+"Annie",
+"Antenna Morty",
+"Antenna Rick",
+"Ants in my Eyes Johnson"
+]
 
 export default function SearchForm() {
- 
+ const [search, setSearch] = useState("");
+ const [searchResults, setSearchResults] = useState([]);
+ const handleChange = e => {
+   setSearch(e.target.value);
+ };
+ useEffect(() => {
+   const results = characters.filter(character => 
+    character.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(results);
+ }, [search]);
   return (
     <section className="search-form">
-     // Add a search form here
+     <input 
+     type = "text"
+     placeholder = "Search"
+     value = {search}
+     onChange = {handleChange}
+     />
+     <ul>
+        {searchResults.map(item => (
+          <li>{item}</li>
+        ))}
+     </ul>
     </section>
   );
 }
