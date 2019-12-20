cf3423ae7993f7017ccb191cc4a871fcbea50b3a
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2019-12-20 11:37:38 -0700

    I got the API working and am trying to get the routes to work.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./components/Header.js";
-
-
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList";
 export default function App() {
   return (
     <main>
-      <Header />
+      <Header /> 
+      <Router>
+          <Route exact path = "/Characters">
+            <CharacterList />
+          </Route>
+          <Route exact path = "/">
+            <WelcomePage />
+          </Route>
+      </Router>
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CharacterList() {
+const CharacterList = (props) => {
   // TODO: Add useState to track data from useEffect
-
+const [character, setCharacter] = useState([]);
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then(res => {
+      console.log(res);
+      setCharacter(res.data.results);
+    })
+    .catch(err => {
+      console.error(err);
+    });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {character.map(characters => (
+          <CharacterDetails key = {characters.id} characters = {characters} />
+      ))}
     </section>
   );
 }
+
+function CharacterDetails({characters}) {
+  const {name, status, species} = characters;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h2>{status}</h2>
+      <h2>{species}</h2>
+    </div>
+  );
+}
+
+export default CharacterList;
